e4b007a61a336053941e1f348f3343d05a3d447a
Serhiy Gerych <serhii13herychfrontend@gmail.com>
2026-09-04 07:09:46 +0300

    feat: update index component to display media types and add TypeScript as a dev dependency

--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,9 +1,17 @@
+import { TYPE_LABELS } from '@app/constants'
+import { MEDIA_TYPES } from '@app/types'
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function Index() {
 	return (
 		<View style={styles.container}>
-			<Text>Hello, World!</Text>
+			<Text style={styles.title}>Hello, World!</Text>
+
+			<Text>
+				{MEDIA_TYPES.map(type => (
+					<Text style={styles.item}>{TYPE_LABELS[type]}, </Text>
+				))}
+			</Text>
 		</View>
 	)
 }
@@ -13,5 +21,15 @@ const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'center',
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		marginBottom: 16,
+	},
+	item: {
+		fontSize: 16,
+		color: 'gray',
+		paddingVertical: 4,
 	},
 })
