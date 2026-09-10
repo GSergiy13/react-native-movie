f7021e0ce1c59df8b8e23f05ca07d96d432397a8
Serhiy Gerych <serhii13herychfrontend@gmail.com>
2026-09-10 15:23:42 +0300

    feat: add button component and integrate it into the index screen; implement search layout and functionality

--- apps/mobile/src/app/(tabs)/index.tsx
+++ apps/mobile/src/app/(tabs)/index.tsx
@@ -1,13 +1,40 @@
+import { Download, Play, Plus } from 'lucide-react-native'
 import { StyleSheet, Text, View } from 'react-native'
 
 import { MEDIA_TYPES } from '@app/types'
 
 import { TYPE_LABELS } from '@app/constants'
 
+import { Button } from '@/components/Button'
+
 export default function Index() {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>Hello, World!</Text>
+
+			<View style={styles['button-container']}>
+				<Button
+					icon={Play}
+					size='md'
+					onPress={() => {}}
+				>
+					Watch video
+				</Button>
+				<Button
+					icon={Download}
+					size='md'
+					variant='secondary'
+					onPress={() => {}}
+				>
+					Download
+				</Button>
+				<Button
+					icon={Plus}
+					size='md'
+					variant='secondary'
+					onPress={() => {}}
+				/>
+			</View>
 
 			<Text>
 				{MEDIA_TYPES.map(type => (
@@ -24,9 +51,18 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center'
 	},
+	'button-container': {
+		display: 'flex',
+		flexDirection: 'row',
+		gap: 8,
+		alignItems: 'center',
+		justifyContent: 'center',
+		marginBottom: 20
+	},
 	title: {
 		fontSize: 20,
 		fontWeight: 'bold',
+		color: 'white',
 		marginBottom: 16
 	},
 	item: {
